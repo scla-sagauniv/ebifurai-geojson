65d08321fbc85de1d1ba20c7587ff1f6c461f5c6
shunsuke-tamura <wwwxx4869@gmail.com>
2023-08-27 11:26:28 +0900

    update: geojson扱うサーバ実装

---
+++ geojson.py
@@ -0,0 +1,48 @@
+import math
+import random
+import cv2
+import numpy as np
+import geopandas as gpd
+from shapely.geometry import MultiPolygon
+# from geojson import Polygon, Feature, FeatureCollection, dump
+
+
+geojson_path = "test2.geojson"
+gdf = gpd.read_file(geojson_path)
+
+
+def load_contour_by_NAME_JA(name: str) -> np.ndarray:
+    polygon = gdf[gdf["NAME_JA"] == name].iloc[0]["geometry"]
+    if type(polygon) == MultiPolygon:
+        polygons = polygon.geoms
+        polygon = max(polygons, key=lambda p: p.area)
+    return np.array(polygon.exterior.coords)
+
+def get_rondom_country_data():
+    random_index = random.randint(0, len(gdf) - 1)
+    random_record = gdf.iloc[random_index]
+    return random_record
+
+def compare_contour(name_ja1, name_ja2):
+    country1_contour = load_contour_by_NAME_JA(name_ja1)
+    country2_contour = load_contour_by_NAME_JA(name_ja2)
+    similarity_score = cv2.matchShapes(
+        country1_contour, country2_contour, cv2.CONTOURS_MATCH_I1, 0
+    )
+    return math.floor((100 - similarity_score) * 100)
+
+
+# トルクメニスタン、ウズベキスタン、アフガニスタン、パキスタンの比較はわかりやすいかも
+# country1_contour = load_contour_by_NAME_JA("オーストラリア")
+# country2_contour = load_contour_by_NAME_JA("トルクメニスタン")
+
+# similarity_score = cv2.matchShapes(
+#     country1_contour, country2_contour, cv2.CONTOURS_MATCH_I1, 0
+# )
+# print(similarity_score)
+
+# feature = Feature(
+#     geometry=Polygon(coordinates=[country1_contour.tolist()]), properties={}
+# )
+# with open("output.geojson", "w") as f:
+#     dump(feature, f, indent=2)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
+from geojson import get_rondom_country_data, compare_contour
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,3 +8,32 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+router = APIRouter(prefix="/geojson", tags=["article"])
+
+
+class QuestionRes(BaseModel):
+    NAME_JA: str
+
+@router.get("/question", response_model=QuestionRes)
+async def question():
+    record = get_rondom_country_data()
+    country: str = record["NAME_JA"]
+    return {"NAME_JA": country}
+
+
+class CompareReq(BaseModel):
+    question_country: str
+    choiced_country: str
+
+class CompareRes(BaseModel):
+    score: int
+
+@router.post("/compare", response_model=CompareRes)
+async def compare(req: CompareReq):
+    score: int = compare_contour(req.choiced_country, req.question_country)
+    return {"score": score}
+    
+
+app.include_router(router)
+
